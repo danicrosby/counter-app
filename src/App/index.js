00cdeb2eb14a5dab4ff1be40a6753580b18243c0
Dani Crosby <dani.crosby1111@gmail.com>
2021-05-22 17:45:02 -0500

    mapping over objects

--- src/App/index.js
+++ src/App/index.js
@@ -3,9 +3,32 @@ import './App.scss';
 import Counter from '../Counter';
 
 function App() {
+  const array = [
+    {
+      appName: 'Counter 1',
+      initialValue: 200
+    },
+    {
+      appName: 'Counter 2',
+      initialValue: 0
+    },
+    {
+      appName: 'Counter 3',
+      initialValue: 100
+    },
+  ];
+
   return (
-    <Counter />
+    <div>
+      { array.map((counter) => <Counter
+        appName={counter.appName}
+        initialValue={counter.initialValue}/>)
+      }
+    </div>
   );
 }
 
 export default App;
+
+// Props are arguemnts: you can customize each Counter component called
+// To call you have to pass it a prop/argument
--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
 
-export default function Counter() {
-  const [counter, setCounter] = useState(0);
+export default function Counter({ appName, initialValue }) {
+  const [counter, setCounter] = useState(initialValue);
   const [userInput, setUserInput] = useState(0);
-  const [counterName, setCounterName] = useState('Counter');
+  const [counterName, setCounterName] = useState(appName);
 
   return (
     <div className='App'>
       <input onChange={(e) => setCounterName(e.target.value)}></input>
       <h1>{counterName}</h1>
       <h2>{counter}</h2>
-
       <input onChange={(e) => setUserInput(Number(e.target.value))}></input><br />
-
-      {/* <button onClick={() => setCounter((prevState) => prevState + 1)}>Increment By 1</button> */}
       <button onClick={() => setCounter((prevState) => prevState + userInput)}>Increment By {userInput}</button>
 
-      {/* {counter <= 0 ? ''
-        : <button onClick={() => setCounter((prevState) => prevState - 1)}>Decrease By 1</button>} */}
       {counter <= 0 ? ''
         : <button onClick={() => setCounter((prevState) => prevState - userInput)}>Decrease By {userInput}</button>}
 
@@ -25,3 +20,12 @@ export default function Counter() {
     </div>
   );
 }
+
+//  <button onClick={() => setCounter((prevState) => prevState + 1)}>Increment By 1</button>
+
+// counter <= 0 ? ''
+//   : <button onClick={() => setCounter((prevState) => prevState - 1)}>Decrease By 1</button>}
+
+// Props is the argument from the index.js page that is customized in the return statement
+
+// destructure props with appName
